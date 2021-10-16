57e06ba5a8ea41d04250e013f47d83c2bf57a985
audreyzhu <zhunr@bc.edu>
2021-10-15 23:50:49 -0400

    final upload

--- AreaChart.js
+++ AreaChart.js
@@ -1,43 +1,47 @@
 export default function AreaChart(container){
 
 	// initialization
-    const margin = {top: 20, right: 20, bottom: 20, left: 50};
+    const margin = {top: 50, right: 20, bottom: 20, left: 50};
     const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const height = 300 - margin.top - margin.bottom;
 
-    const svg = d3.select(container).append('svg')
-        .attr('width', width + margin.left + margin.right)
+    var svg = d3.select(container).append('svg')
+        .attr('width', width + margin.left + margin.right) //.attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    const xScale = d3.scaleTime()
+    var xScale = d3.scaleTime()
         .range([0,width]);
-    
-    const yScale = d3.scaleLinear()
-        .range([height, 0]);
+    var yScale = d3.scaleLinear()
+        .rangeRound([height, 0]);
 
     let xAxis = d3.axisBottom()
         .scale(xScale);
-
     let yAxis = d3.axisLeft()
         .scale(yScale);
-
     let xAxisGroup = svg.append('g')
         .attr('class', 'axis x-axis');
-
     let yAxisGroup = svg.append('g')
         .attr('class', 'axis y-axis');
-    //add labels? 
 
-    svg.append('path').attr('class', 'areapath');
+    svg.append('path')
+        .attr('class', 'areapath')
+        .attr('fill', d3.schemeTableau10[0]);
+
+    svg.append('text')
+        .attr('x', -20)
+        .attr('y', -20)
+        .attr('font-size', 14)
+        .text('Total Unemployment')
+        .attr('fill', '#5b5b5b');
 
     
 	function update(data){ 
 
 		// update scales, encodings, axes (use the total count)
         xScale.domain(d3.extent(data, d=> d.date));
-        yScale.domain(d3.extent(data, d=> d.total));
+        yScale.domain([0, d3.max(data, d=> d.total)]);
 
         var area = d3.area()
             //.datum(data)
@@ -46,15 +50,12 @@ export default function AreaChart(container){
             .y0(yScale(0))
             //.attr('d', area)
         
-        svg.select('areapath')
+        d3.select('.areapath')
             .datum(data)
             .attr('d', area);
-        
-
-
-            
-        xAxis = d3.axisBottom(xScale)
-            .ticks(5, 's');
+             
+        xAxis = d3.axisBottom()
+            .scale(xScale);
 
         yAxis = d3.axisLeft()
             .scale(yScale);
@@ -65,13 +66,32 @@ export default function AreaChart(container){
         
         yAxisGroup = svg.select('.y-axis')
             .call(yAxis);
-        
-        console.log(xScale.domain());
+
 	}
 
+    var brush = d3.brushX()
+        .extent([[0,0], [width,height]])
+        .on('brush', brushed)
+        .on('end', brushed)
+
+    svg.append('g')
+        .attr('class', 'brush')
+        .call(brush);
+
+    var listeners = { brushed: null }
+
+    function on(event, listener) {
+        listeners[event] = listener;
+    }
+
+    function brushed(event) {
+        if (event.selection) {
+            listeners['brushed'](event.selection.map(xScale.invert));
+        }
+    }
 
 	return {
-		update // ES6 shorthand for "update": update
+		update, on // ES6 shorthand for "update": update
 	};
 }
 
--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 import AreaChart from './AreaChart.js';
-//import StackedAreaChart from './StackedAreaChart.js';
+import StackedAreaChart from './StackedAreaChart.js';
 
 let unemployeddata;
 
 
-d3.csv('unemployment.csv', d=> {
-    return {
-        ...d, 
-        date: Date(d.date),
-    }
-}).then(data => {
+d3.csv('unemployment.csv', d3.autoType).then(data => {
     unemployeddata = data;
-    //unemployeddata[0].date = Date(data[0].date);
-    console.log(data)
+    console.log(data);
 
-    const overview = AreaChart('.chart');
-    overview.update(data);  
+    var categories = data.columns.slice(1, -1);
+    unemployeddata.categories = categories;
+    
+    for (let i=0; i < data.length; i++) {
+        unemployeddata[i].date = new Date(data[i].date);
+        //unemployeddata[i].total = parseInt(data[i].total);
+    }
+
+
+    var area_chart = AreaChart('.area-chart');
+    area_chart.update(unemployeddata);  
+    area_chart.on('brushed', function(range) {
+        stacked_chart.filterByDate(range);
+    });
+    
+
+    var stacked_chart = StackedAreaChart('.stacked-chart');
+    stacked_chart.update(unemployeddata);
+
 });
 
